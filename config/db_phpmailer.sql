2f3e94cf977310c5efed3d02607a0a90805a11c0
komangyxy-commits <ikomangriskytriyanaputra@gmail.com>
2026-06-17 22:29:24 +0800

    update db

--- config/db_phpmailer.sql
+++ config/db_phpmailer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 17, 2026 at 10:54 AM
+-- Generation Time: Jun 17, 2026 at 02:27 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
